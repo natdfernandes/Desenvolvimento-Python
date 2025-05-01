47ea27ebcf6b28e173f6f3f1a33752861aa54c88
Natalia <nataliadiasfernande@gmail.com>
2025-05-01 09:50:00 -0300

    estudo sobre pandas e extração de dados

--- pandas/main.py
+++ pandas/main.py
@@ -6,4 +6,22 @@ tabelas = pd.read_html(
     "https://pt.wikipedia.org/wiki/Lista_de_filmes_de_maior_bilheteria"
 )
 # Lista com as tabelas
-tabelas
+# print(tabelas)
+
+# Pegando a primeira tabela da lista
+tabela = tabelas[0]
+# Visualizando tabela
+# print(tabela)
+
+# Filtro a tabela com as colunas que quero
+tabela_filtrada = tabela[["Diretor(a)", "Bilheteria (US$)"]]
+# print(tabela_filtrada)
+# Trato os meus dados e transforma em valores numéricos
+tabela_filtrada["Bilheteria (US$)"] = (
+    tabela_filtrada["Bilheteria (US$)"]
+    .str.replace(" ", "")
+    .str.replace(".", "")
+    .astype("int64")
+)
+# Faço o agrupamento por diretor
+print(tabela_filtrada.groupby("Diretor(a)").sum())
